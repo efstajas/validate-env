eabac3f7358160e16a5d72579fea3f9ebf9dc971
Jason Efstathiou <j@jason-e.dev>
2020-05-03 16:05:21 +0200

    reject only if something actually went wrong

--- src/validator.ts
+++ src/validator.ts
@@ -4,53 +4,47 @@ import { ValidatorResult, VariableType, FailReason } from './validator.types'
 import validateVar from './methods/validateVar';
 
 export default (location: string): Promise<ValidatorResult> => {
-  let readInterface: readline.Interface;
-
-  try {
-    readInterface = readline.createInterface({
-      input: fs.createReadStream(location),
-    })
-  } catch (e) {
-    throw new Error(`Error reading template file at provided path ${location}. Please ensure the path is correct.`)
-  }
-
   return new Promise((resolve, reject) => {
-    readInterface
-      .on('line', (line) => {
-        if (!line || line === '') return
+    const readInterface = readline.createInterface({
+      input: fs.createReadStream(location).on('error', () => {
+        return reject(new Error(`Couldn't open file at ${location}. Please ensure you're passing the right path to your env template.`))
+      }),
+    })
 
-        const name = line.split('=')[0]
-        const expectedType = line.split('=')[1].toUpperCase()
-        const actualValue = process.env[name]
+    readInterface.on('line', (line) => {
+      if (!line || line === '') return
 
-        if (!Object.values(VariableType).includes(expectedType as any)) {
-          throw new Error(`Var ${name} has unknown expected type ${expectedType}. Valid types: string, number, array`)
-        }
+      const name = line.split('=')[0]
+      const expectedType = line.split('=')[1].toUpperCase()
+      const actualValue = process.env[name]
 
-        const result = validateVar(expectedType as VariableType, actualValue)
+      if (!Object.values(VariableType).includes(expectedType as any)) {
+        return reject(new Error(`Var ${name} has unknown expected type ${expectedType}. Valid types: string, number, array`))
+      }
 
-        if (!result.pass) {
-          reject({
-            result: 'fail',
-            failedVar: result.failReason === FailReason.MISSING
-              ? {
-                reason: FailReason.MISSING,
-                name,
-                expectedType: expectedType as VariableType,
-                valid: false
-              } : {
-                reason: FailReason.WRONG_TYPE,
-                name,
-                expectedType: expectedType as VariableType,
-                valid: false
-              },
-          } as ValidatorResult)
-        }
-      })
-      .on('close', () => {
+      const result = validateVar(expectedType as VariableType, actualValue)
+
+      if (!result.pass) {
         return resolve({
-          result: 'pass'
-        })
+          result: 'fail',
+          failedVar: result.failReason === FailReason.MISSING
+            ? {
+              reason: FailReason.MISSING,
+              name,
+              expectedType: expectedType as VariableType,
+              valid: false
+            } : {
+              reason: FailReason.WRONG_TYPE,
+              name,
+              expectedType: expectedType as VariableType,
+              valid: false
+            },
+        } as ValidatorResult)
+      }
+    }).on('close', () => {
+      return resolve({
+        result: 'pass'
       })
+    })
   })
 }
